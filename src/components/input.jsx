b84ee98c3afe8f9a96c5edd0f76ad88036198ebf
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-10-15 13:51:46 +1100

    add input field

---
+++ src/components/input.jsx
@@ -0,0 +1,9 @@
+export default function Inputs() {
+  return (
+    <div className="input-field">
+      <input type="text" name="" id="" placeholder="hello" />
+      <input type="text" name="" id="" placeholder="hello" />
+      <input type="text" name="" id="" placeholder="hello" />
+    </div>
+  );
+}
