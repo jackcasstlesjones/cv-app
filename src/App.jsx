65b3963399c1499dc55ef2fc921674af0391267b
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-10-15 14:56:58 +1100

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,20 @@ function App() {
   };
 
   return (
-    <div className="big-container">
-      <Inputs setHeadCont={handleHeaderChange} />
-      <DisplayCV headerContent={headCont} />
-    </div>
+    <>
+      <head>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <title>Your React App</title>
+      </head>
+      <div className="big-container">
+        <Inputs setHeadCont={handleHeaderChange} />
+        <DisplayCV headerContent={headCont} />
+      </div>
+    </>
   );
 }
 
