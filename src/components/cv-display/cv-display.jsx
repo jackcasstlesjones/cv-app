1de81b305bc5875b60f375f87f9b830dc0e66e55
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-10-17 18:08:40 +1100

    change empty strings to null

--- src/components/cv-display/cv-display.jsx
+++ src/components/cv-display/cv-display.jsx
@@ -21,21 +21,21 @@ export default function DisplayCV({
     <>
       <div className="cv-container">
         <div className="cv-header">
-          <h1>{personalSubmitted ? fullName : ""}</h1>
+          <h1>{personalSubmitted ? fullName : null}</h1>
           <div className="email-phone-container">
             <p className="email-address">
-              Email: {personalSubmitted ? email : ""}
+              Email: {personalSubmitted ? email : null}
             </p>
             <p className="phone-number">
-              Phone Number: {personalSubmitted ? phone : ""}
+              Phone Number: {personalSubmitted ? phone : null}
             </p>
           </div>
         </div>
         <div className="cv-body">
           <h2>Education</h2>
-          <p>University: {educationSubmitted ? university : ""}</p>
-          <p>Degree Title: {educationSubmitted ? degree : ""}</p>
-          <p>Dates Studied: {educationSubmitted ? dates : ""}</p>
+          <p>University: {educationSubmitted ? university : null}</p>
+          <p>Degree Title: {educationSubmitted ? degree : null}</p>
+          <p>Dates Studied: {educationSubmitted ? dates : null}</p>
         </div>
       </div>
     </>
